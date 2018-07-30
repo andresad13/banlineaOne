35a8575ec294ef6edd2274cf5120d675d01fc4f6
andresad13 <andresad13@gmail.com>
2018-07-30 00:04:07 -0500

    update login
    login screen, splash screen

--- OneDemo/app/src/main/java/com/banlinea/one/onedemo/MainActivity.java
+++ OneDemo/app/src/main/java/com/banlinea/one/onedemo/MainActivity.java
@@ -1,13 +1,44 @@
 package com.banlinea.one.onedemo;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+import com.github.ybq.android.spinkit.style.CubeGrid;
+import com.github.ybq.android.spinkit.style.DoubleBounce;
 
 public class MainActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        ProgressBar progressBar = (ProgressBar)findViewById(R.id.spin_kit);
+        CubeGrid cubeGrid = new CubeGrid();
+        progressBar.setIndeterminateDrawable(cubeGrid);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(MainActivity.this,Login.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
     }
 }
